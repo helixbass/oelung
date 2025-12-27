216c3137a54466966f6ad2738d11e76f607bacb6
Julian Rosse <julian@helixbass.net>
2025-12-27 13:53:56 -0500

    working on component

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -1,9 +1,10 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-
+use squalid::{OptionExtDefault, _d};
 use syn::{
+    bracketed,
     parse::{Parse, ParseStream, Result},
-    parse_macro_input, Ident, Token,
+    parse_macro_input, Ident, LitFloat, Token,
 };
 
 enum Element {
@@ -28,7 +29,7 @@ impl Parse for Element {
                 }
                 input.parse()?
             }),
-            key => return Err(input.error(format!("Unexpected key `{key}`"))),
+            key => return Err(input.error(format!("Unexpected element `{key}`"))),
         })
     }
 }
@@ -41,11 +42,39 @@ impl ToTokens for Element {
 
 struct FlexColumn {
     pub children: Vec<Element>,
+    pub flex_grow: Option<LitFloat>,
 }
 
 impl Parse for FlexColumn {
     fn parse(input: ParseStream) -> Result<Self> {
-        unimplemented!()
+        let mut children: Option<Vec<Element>> = _d();
+        let mut flex_grow: Option<LitFloat> = _d();
+
+        while input.peek(Ident) {
+            let key = input.parse::<Ident>().unwrap().to_string();
+            match &*key {
+                "children" => {
+                    assert!(children.is_none(), "Already saw 'children' key");
+                    let children_content;
+                    bracketed!(children_content in input);
+                    let children = children.populate_default();
+                    while !children_content.is_empty() {
+                        children.push(children_content.parse()?);
+                        children_content.parse::<Option<Token![,]>>()?;
+                    }
+                }
+                "flex_grow" => {
+                    assert!(flex_grow.is_none(), "Already saw 'flex_grow' key");
+                    flex_grow = Some(input.parse()?);
+                }
+                key => return Err(input.error(format!("Unexpected key `{key}`"))),
+            }
+        }
+
+        Ok(Self {
+            children: children.expect("Expected `children`"),
+            flex_grow,
+        })
     }
 }
 
