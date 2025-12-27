4879927fb8f9caa6f184c91bc85e6300ac9df8b4
Julian Rosse <julian@helixbass.net>
2025-12-27 13:43:04 -0500

    working on component

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -3,19 +3,48 @@ use quote::{quote, ToTokens};
 
 use syn::{
     parse::{Parse, ParseStream, Result},
-    parse_macro_input,
+    parse_macro_input, Ident, Token,
 };
 
-enum Element {}
+enum Element {
+    FlexColumn(FlexColumn),
+    Text(Text),
+    Cursor(Cursor),
+}
 
 impl Parse for Element {
     fn parse(input: ParseStream) -> Result<Self> {
-        unimplemented!()
+        input.parse::<Token![%]>()?;
+        let name: Ident = input.parse()?;
+
+        Ok(match &*name.to_string() {
+            "FlexColumn" => Self::FlexColumn(input.parse()?),
+            "Text" => Self::Text(input.parse()?),
+            "Cursor" => Self::Cursor({
+                input.parse::<Token![.]>()?;
+                let relative: Ident = input.parse()?;
+                if relative.to_string() != "Relative" {
+                    return Err(input.error(format!("Expected `Relative`")));
+                }
+                input.parse()?
+            }),
+            key => return Err(input.error(format!("Unexpected key `{key}`"))),
+        })
     }
 }
 
 impl ToTokens for Element {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        unimplemented!()
+    }
+}
+
+struct FlexColumn {
+    pub children: Vec<Element>,
+}
+
+impl Parse for FlexColumn {
+    fn parse(input: ParseStream) -> Result<Self> {
         unimplemented!()
     }
 }
