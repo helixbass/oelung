bcbc3b6f11c828c0590d0288d5b4c574df577f09
Julian Rosse <julian@helixbass.net>
2025-12-27 14:25:53 -0500

    working on component

--- examples/flex_column.rs
+++ examples/flex_column.rs
@@ -1,6 +1,6 @@
 use crossterm::event::{self, Event, KeyCode};
 
-use oelung::{Cursor, Renderer, TextBuilder, soft};
+use oelung::{soft, Cursor, Renderer, TextBuilder};
 
 fn main() -> Result<(), anyhow::Error> {
     let mut renderer = Renderer::try_new()?;
@@ -19,53 +19,21 @@ fn main() -> Result<(), anyhow::Error> {
 
 fn render_screen(renderer: &mut Renderer) -> Result<(), anyhow::Error> {
     renderer.render(soft! {
-        %FlexColumn
+      %FlexColumn
+        children => [
+          %FlexColumn
             children => [
-                %FlexColumn
-                    children => [
-                        %Text
-                            text => "Top area"
-                            cursor => %Cursor.Relative
-                                x => 0
-                                y => 0
-                    ]
-                    flex_grow => 1
-                %Text "some_file.rs [1%] 999 lines |1"
-                %Text "This looks great. Hit q to quit"
+              %Text
+                text => "Top area"
+                cursor => %Cursor.Relative
+                  x => 0
+                  y => 0
             ]
+            flex_grow => 1
+          %Text "some_file.rs [1%] 999 lines |1"
+          %Text "This looks great. Hit q to quit"
+        ]
     })?;
-
-    Ok(())
-}
-    renderer.render(
-        FlexColumnBuilder::default()
-            .child(
-                FlexColumnBuilder::default()
-                    .child(
-                        TextBuilder::default()
-                            .text_child("Top area")
-                            .cursor_child(Cursor::relative().x(0).y(0))
-                            .build()?
-                            .into(),
-                    )
-                    .flex_grow(1)
-                    .build()?
-                    .into(),
-            )
-            .child(
-                TextBuilder::default()
-                    .text_child("some_file.rs [1%] 999 lines |1")
-                    .build()?
-                    .into(),
-            )
-        TextBuilder::default()
-            .text_child("Hello world")
-            .text_child(" ")
-            .text_child("(hit q to quit)")
-            .cursor_child(Cursor::relative().x(0).y(0))
-            .build()?
-            .into(),
-    )?;
 
     Ok(())
 }
