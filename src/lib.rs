b02f03f31163f4a5f415563a0845f875dfa0f262
Julian Rosse <julian@helixbass.net>
2025-12-28 15:08:21 -0500

    working on component

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ pub use proc_macros::soft;
 pub use crossterm;
 
 mod component;
-mod crossterm;
 mod cursor;
 mod error;
 mod flex;
 mod render;
 mod style;
+mod terminal;
 mod text;
 
 pub use component::{Component, ComponentInterface};
-pub use crossterm::{size, take_over_screen, Size, TakeOverScreenGuard};
 pub use cursor::{Cursor, Offset};
 pub use error::Error;
 pub use flex::{FlexColumn, FlexColumnBuilder};
 pub use render::{Grid, Renderer};
 pub use style::{Style, StyleBuilder};
+pub use terminal::{size, take_over_screen, Size, TakeOverScreenGuard};
 pub use text::{Text, TextBuilder, TextChild, TextChildren};
